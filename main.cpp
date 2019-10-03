cdfdca9f0ccdddf7ea08e63cb9ddf315a96d38d4
Greyvin Tercero Ruiz <53482798+Origamy4321@users.noreply.github.com>
2019-10-02 21:59:41 -0500

    Finalizado

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 //
 // Created by Lady Asura III on 20/9/2019.
 //
+#include <iostream>
 #include "ListaVehiculos.h"
-
+using namespace std;
 int main(){
 
 Automovil automovil1("hyundai","accent",2009,567123,5000,20);
 Motocicleta motocicleta1("Yamaha", "1",2005,561231,3000,10);
 
 ListaVehiculos *listaVehiculos = new ListaVehiculos();
+listaVehiculos->insertarInicio(motocicleta1, automovil1);
+cout<< listaVehiculos->toString();
 
 
 
